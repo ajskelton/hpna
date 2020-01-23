9192fe7510a00f950f15434205071b0877a98171
Anthony <anthony@redcartstudios.com>
2020-01-22 21:21:37 -0800

    Finished Primary Navigation Hover States and homepage cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
 			sm: '640px',
 			md: '768px',
 			lg: '1024px',
-			xl: '1024px',
+			xl: '1240px',
 		},
 		extend: {
 			colors: {
--- template-parts/block/content-cta-card.php
+++ template-parts/block/content-cta-card.php
@@ -6,9 +6,9 @@ $link = get_field( 'link' );
 ?>
 
 <div class="c-card c-card--cta">
-    <a href="<?php echo esc_url( $link['url'] ); ?>">
+    <a class="c-card__wrapper" href="<?php echo esc_url( $link['url'] ); ?>">
         <div class="c-card__top">
-            <figure class="w-64 mx-auto">
+            <figure class="w-40 mx-auto">
 	            <?php echo hpna_load_inline_svg( "/icon-{$icon}.svg" ); ?>
             </figure>
         </div>
