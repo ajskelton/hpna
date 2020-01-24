35d4fe79a3d18522062ce8d8bc8ccb892c71a2c7
Anthony <anthony@redcartstudios.com>
2020-01-23 17:20:21 -0800

    Updates tailwind config for 62.5% html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
 			lg: '1024px',
 			xl: '1240px',
 		},
+		fontSize: {
+			'xs': '1.2rem',
+			'sm': '1.4rem',
+			'base': '1.6rem',
+			'lg': '1.8rem',
+			'xl': '2rem',
+			'2xl': '2.4rem',
+			'3xl': '3rem',
+			'4xl': '3.6rem',
+			'5xl': '4.8rem',
+			'6xl': '6.4rem',
+		},
 		extend: {
 			colors: {
 				lightGreen: '#66CC33',
@@ -20,7 +32,7 @@ module.exports = {
 		},
 	},
 	variants: {
-		sisibility: ['group-hover']
+		visibility: ['group-hover']
 	},
 	plugins: []
 }
