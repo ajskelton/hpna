9e902caa903588a6398c611de5753ad2cbde4852
Anthony <anthony@redcartstudios.com>
2020-01-25 14:51:01 -0800

    Adds Archive Template and updates page title

--- template-parts/content-page-header.php
+++ template-parts/content-page-header.php
@@ -1,3 +1,12 @@
+<?php
+
+    if ( is_archive() ) {
+        $page_title = get_the_archive_title();
+    } else if ( is_search() ) {
+        $page_title = '<strong>Search for:</strong> ' . get_search_query();
+    }
+?>
+
 <div class="wp-block-cover alignfull has-green-background-color has-background-dim is-style-page-title"><div class="wp-block-cover__inner-container">
         <h1 class="has-text-align-center"><?php echo wp_kses_post( $page_title ); ?></h1>
     </div>
