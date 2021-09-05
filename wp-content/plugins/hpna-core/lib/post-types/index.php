fd63a3407e75af1495a6ce86920d8cbc48a096c9
Anthony <anthony@redcartstudios.com>
2021-09-05 16:46:23 -0700

    Checks in Core Plugin
    There are seven commits of history we lose by doing it this way, but there's
    nothing really significant in there so I'm going with the easy route to merge
    the two repos.

---
+++ wp-content/plugins/hpna-core/lib/post-types/index.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Post Types
+ */
+//include_once( 'events.php' );
+include_once( 'newsletters.php' );
+include_once( 'financial-reports.php' );
+include_once( 'events.php' );
+include_once( 'meeting-minutes.php' );
